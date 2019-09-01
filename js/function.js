2684ff9414984f362a09c18abb777bf8ae8480f5
Md-Mudassir <mdmudassirpro@gmail.com>
2019-09-01 21:36:04 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,8 +1,16 @@
 //to display the celcius and farhenite option
 let CF = `<sup id="C" onclick="myWeather.getconvertTempC()"> &degC |</sup><sup id="F" onclick="myWeather.getconvertTempF()">&degF</sup>`;
-
+let city = document.getElementById("city");
+let data = document.getElementById("data");
+let temp = document.getElementById("temp");
+let input = document.getElementById("pick").value;
 //function to retrieve the elements
-class weatherForecast {
+class WeatherForecast {
+  constructor() {
+    this.city = city;
+    this.temp = temp;
+    this.data = data;
+  }
   weatherApp() {
     let cities = [
       { city: "Bangalore, Karnataka", Celcius: 25, Data: "Sunny ☀️" },
@@ -10,31 +18,37 @@ class weatherForecast {
       { city: "Adoni, Andhra Pradesh", Celcius: 30, Data: "Cloudy ☁️" },
       { city: "Bellary, Karnataka", Celcius: 10, Data: "Fog 🌫️" }
     ];
-    let city = document.getElementById("city");
-    let data = document.getElementById("data");
-    let temp = document.getElementById("temp");
-    let input = document.getElementById("pick").value;
+
     let filtred = cities
       .filter(n => n.city.includes(input))
       .map(v => {
-        (city.innerHTML = v.city),(temp.innerHTML = v.Celcius + CF),(data.innerHTML = v.Data);
+        (city.innerHTML = v.city),
+          (temp.innerHTML = v.Celcius + CF),
+          (data.innerHTML = v.Data);
       });
   }
 }
+
+const myWeather = new WeatherForecast();
+console.log(this);
+
+class Myweather2 extends WeatherForecast {
+  constructor() {
+    super();
+    console.log(this.temp);
+  }
+}
 // to convert C to F and viceversa
-weatherForecast.prototype.getconvertTempF = function() {
-  let C = this.temp;
+WeatherForecast.prototype.getconvertTempF = function() {
+  let C = temp;
   let Fer = Math.round((C * 9) / 5 + 32);
   document.getElementById("temp").innerHTML = Fer + CF;
 };
 
-weatherForecast.prototype.getconvertTempC = function() {
-  let C = this.temp;
+WeatherForecast.prototype.getconvertTempC = function() {
+  let C = temp;
   document.getElementById("temp").innerHTML = C + CF;
 };
-
-const myWeather = new weatherForecast();
-console.log(this.temp);
 
 // day and time functions
 function addZero(i) {
